fba2773426976187ff53b990b408ffdd331edc8f
Michael Cole <colemichaelj@yahoo.com>
2023-01-08 21:41:26 -0600

    first commit

---
+++ dimensions.py
@@ -0,0 +1,5 @@
+import math
+
+def circle(radius):
+    r = 2*radius*(math.pi)
+    return r
---
+++ module.py
@@ -0,0 +1,13 @@
+from math import pi 
+
+# 1) Has a function to calculate the square footage of a house 
+# Reminder of Formula: Length X Width == Area
+# 2) Has a function to calculate the circumference of a circle 
+
+def houseArea(l, w):
+    a = l * w
+    return a
+
+def circle(radius):
+    r = 2*radius
+    return r
